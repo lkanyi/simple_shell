18e336e5ad88b6c64da842c2437986786024f4c5
leah kanyi <kanyileah46@gmail.com>
2022-05-18 19:09:48 +0300

    update header file

--- main.h
+++ main.h
@@ -2,4 +2,11 @@
 #define MAIN_H
 
 
+int _exec(char *command, char **cmd, char **env)
+void _exit_func(char **environ)
+char *find_path(char **environ)
+int _fork(char *myself, command_t *cmd_node, char *path, char **env)
+char *command_t **_prompt(char *myself, char *argv)
+int main(int ac, char **av, char **env)
+
 #endif
